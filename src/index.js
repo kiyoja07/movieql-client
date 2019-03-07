4b949651fb62d9b4b89e75db98d30486d3d1bdd2
kioyoja07 <kiyoja07@gmail.com>
2019-03-08 02:29:41 +0900

    finished version, completed class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./globalStyles";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // render() method that takes input data and returns what to display
